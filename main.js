c652fd7542131c1381f547748d081c7ef750b0e4
KacperJar <kacperjar231@gmail.com>
2025-06-05 17:10:30 +0000

    Ostatnie poprawki (mam nadzieje)

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { CubeControls } from './cube-controls';
-import { Clock } from 'three/webgpu';
 
 // Scena
 const scene = new THREE.Scene();
